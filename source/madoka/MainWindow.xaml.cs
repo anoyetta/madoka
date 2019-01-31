45c56145bdd9addb1f087b9e62dc5d5668efef5c
anoyetta <anoyetta@gmail.com>
2019-01-31 18:09:47 +0900

    NotifyIcon関連の実装を開始した

--- source/madoka/MainWindow.xaml.cs
+++ source/madoka/MainWindow.xaml.cs
@@ -1,3 +1,6 @@
+using System;
+using System.ComponentModel;
+using System.Windows;
 using MahApps.Metro.Controls;
 
 namespace madoka
@@ -7,9 +10,59 @@ namespace madoka
     /// </summary>
     public partial class MainWindow : MetroWindow
     {
+        public static MainWindow Instance { get; private set; }
+
         public MainWindow()
         {
+            Instance = this;
+
             this.InitializeComponent();
+
+            this.StateChanged += this.MainWindow_StateChanged;
+            this.Closing += this.MainWindow_Closing;
+        }
+
+        private void MainWindow_StateChanged(object sender, EventArgs e)
+        {
+            if (this.WindowState == WindowState.Minimized)
+            {
+                this.Visibility = Visibility.Collapsed;
+            }
+            else
+            {
+                this.Visibility = Visibility.Visible;
+            }
+        }
+
+        private volatile bool toEnd;
+
+        private void MainWindow_Closing(object sender, CancelEventArgs e)
+        {
+            if (!this.toEnd)
+            {
+                this.WindowState = WindowState.Minimized;
+                e.Cancel = true;
+                return;
+            }
+
+            this.NotifyIcon.Visibility = Visibility.Collapsed;
+            this.NotifyIcon.Dispose();
+        }
+
+        public void ToShow()
+        {
+            this.WindowState = WindowState.Normal;
+        }
+
+        public void ToHide()
+        {
+            this.WindowState = WindowState.Minimized;
+        }
+
+        public void ToEnd()
+        {
+            this.toEnd = true;
+            this.Close();
         }
     }
 }
